4a05d2c8870fd02f0994c6565ad08061f3f3f251
cristian <cristianhernandez9007@gmail.com>
2024-07-15 11:43:29 -0400

    deleted all nvim config

--- init.lua
+++
@@ -1,3 +0,0 @@
-require("general")
-require("keymaps")
-require("lazyexec")
--- lua/plugins/guess-indent.lua
+++
@@ -1,6 +0,0 @@
-return {
-	-- smart detect the indent level used for the file
-	"NMAC427/guess-indent.nvim",
-	opts = {},
-	event = "BufRead",
-}
