05bf28161973fcfd0ae3394cd26583c091b51c49
cristian <cristianhernandez9007@gmail.com>
2024-07-17 22:43:09 -0400

    add modified check in status line

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,22 +1,38 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 
 -- load all base46 highlights
 for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
-  dofile(vim.g.base46_cache .. v)
+	dofile(vim.g.base46_cache .. v)
 end
 
 -- base46+ui config
 local M = {}
-M.ui = {
-theme = "rosepine",
 
--- hl_override = {
--- Comment = { italic = true },
--- [@comment] = { italic = true },
--- },
+M.ui = {
+	theme = "rosepine",
+
+	statusline = {
+    theme = "vscode_colored",
+
+		modules = {
+			file_modified = function()
+				-- return vim.bo.modified and "%#St_file#%#St_file_sep#" or ""
+        return vim.bo.modified and "[]" or ""
+
+
+			end,
+		},
+
+		order = {"mode", "file","file_modified", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
+	},
+
+	-- hl_override = {
+	-- Comment = { italic = true },
+	-- [@comment] = { italic = true },
+	-- },
 }
 
 return M
