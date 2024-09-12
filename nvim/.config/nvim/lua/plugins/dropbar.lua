38ed929fe11c6f31875ade105c75d5d836ed55a0
cristian <cristianhernandez9007@gmail.com>
2024-09-12 12:28:57 -0400

    remove dropbar lua file also

--- nvim/.config/nvim/lua/plugins/dropbar.lua
+++
@@ -1,9 +0,0 @@
--- IDE-like breadcrumbs, out of the box.
-return {
-    -- "Bekaboo/dropbar.nvim",
-    -- lazy = false,
-    -- keys = {
-    --     -- pick mode: require('dropbar.api').pick()
-    --     { "<leader>ue", ":lua require('dropbar.api').pick()<CR>", desc = "Pick Mode Dropbar" },
-    -- },
-}
