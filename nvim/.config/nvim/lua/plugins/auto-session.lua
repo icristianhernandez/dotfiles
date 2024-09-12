4667713dabe81ce269c759976183f46951b97d83
cristian <cristianhernandez9007@gmail.com>
2024-09-11 22:23:08 -0400

    added localoptions to session options and update the autosessions new name api

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -4,24 +4,39 @@ return {
         lazy = false,
         dependencies = { "nvim-telescope/telescope.nvim" },
         config = function()
-            vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,terminal,"
+            vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,terminal,localoptions,"
             local neovide_path = "/mnt/c/Program Files/Neovide"
             local is_neovide = vim.loop.cwd() == neovide_path
 
             local enable_last_session = vim.loop.cwd() == vim.loop.os_homedir() or is_neovide
 
+            -- require("auto-session").setup({
+            --     -- auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+            --     -- auto_session_use_git_branch = true,
+            --     auto_session_enable_last_session = enable_last_session,
+            --     auto_save_enabled = true,
+            --     auto_restore_enabled = true,
+            --
+            --     session_lens = {
+            --         buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+            --         load_on_setup = true,
+            --         theme_conf = { border = true },
+            --         previewer = false,
+            --     },
+            -- })
+
             require("auto-session").setup({
-                -- auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-                -- auto_session_use_git_branch = true,
-                auto_session_enable_last_session = enable_last_session,
-                auto_save_enabled = true,
-                auto_restore_enabled = true,
+                auto_restore = true,
+                auto_restore_last_lession = enable_last_session,
+                auto_save = true,
 
                 session_lens = {
-                    buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+                    buftypes_to_ignore = {},
                     load_on_setup = true,
-                    theme_conf = { border = true },
                     previewer = false,
+                    theme_conf = {
+                        border = true,
+                    },
                 },
             })
 
