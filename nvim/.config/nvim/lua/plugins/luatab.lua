02ed9d905ab5e2c5b4127f83139aaa2111ea95d8
cristian <cristianhernandez9007@gmail.com>
2024-10-20 11:38:09 -0400

    removed unused plugins, added luatab as I wanted

--- nvim/.config/nvim/lua/plugins/luatab.lua
+++ nvim/.config/nvim/lua/plugins/luatab.lua
@@ -1,10 +1,13 @@
 return {
-    {
-        "alvarosevilla95/luatab.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        event = "VimEnter",
-        config = function()
-            require("luatab").setup()
-        end,
-    },
+    -- luatab: A simple and clean tabline plugin for neovim
+    "alvarosevilla95/luatab.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VimEnter",
+    config = function()
+        require("luatab").setup({
+            windowCount = function()
+                return " "
+            end,
+        })
+    end,
 }
