5bda8cecb9ad4b9d094b7c710fdd7f61754c6bb1
cristian <cristianhernandez9007@gmail.com>
2025-02-17 20:05:39 -0400

    feat(nvim): fresh cmdline noice format and removed conceal hints in cmdl

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -6,5 +6,26 @@ return {
                 enabled = false,
             },
         },
+
+        cmdline = {
+            format = {
+                filter = false,
+                lua = false,
+                help = false,
+            },
+        },
+
+        views = {
+            cmdline_popup = {
+                border = {
+                    style = "none",
+                    padding = { 1, 2 },
+                },
+                filter_options = {},
+                win_options = {
+                    winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+                },
+            },
+        },
     },
 }
