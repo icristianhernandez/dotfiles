25210c1184218894670cbae3086f4dee9bd0a6a4
cristian <cristianhernandez9007@gmail.com>
2025-03-11 19:09:32 -0400

    refactor(nvim): ditched nvim.autopair for a more powerful autopair setting

--- nvim/.config/nvim/lua/plugins/disabled-lazyvim.lua
+++ nvim/.config/nvim/lua/plugins/disabled-lazyvim.lua
@@ -4,4 +4,5 @@ return {
     { "folke/persistence.nvim", enabled = false },
     { "lewis6991/gitsigns.nvim", enabled = true },
     { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
+    -- { "echasnovski/mini.pairs", enabled = false },
 }
---
+++ nvim/.config/nvim/lua/plugins/ultimate-autopair.lua
@@ -0,0 +1,10 @@
+return {
+    "altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    opts = {
+        space = {
+            -- That can be changed in the future bc it's a good option
+            enable = false,
+        },
+    },
+}
