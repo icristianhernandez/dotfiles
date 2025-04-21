365fcc55ca5979d134913cd576ddc1ad3356aa8f
cristian <cristianhernandez9007@gmail.com>
2025-04-21 18:54:00 -0400

    chore(nvim): comment out tabline configuration
    The tabline configuration in `lualine_a` has been commented out, including the logic to reset `showtabline` to its default behavior. This change disables the tabline display in the lualine plugin.

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,18 +5,15 @@ return {
             -- lualine_a = {
             --     { "filetype", icon_only = true },
             -- },
-            lualine_a = {
-                { "tabs", mode = 2, max_length = vim.o.columns },
-                {
-                    function()
-                        --HACK: lualine will set &showtabline to 2 if you have configured
-                        --lualine for displaying tabline. We want to restore the default
-                        --behavior here.
-                        vim.o.showtabline = 1
-                        return ""
-                    end,
-                },
-            },
+            -- lualine_b = {
+            --     { "tabs", mode = 2, max_length = vim.o.columns },
+            --     {
+            --         function()
+            --             vim.o.showtabline = 1
+            --             return ""
+            --         end,
+            --     },
+            -- },
         },
     },
 }
