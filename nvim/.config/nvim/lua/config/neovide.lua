43f61efd6bd70476b57c0040a35ce0f22f736475
cristian <cristianhernandez9007@gmail.com>
2025-04-25 20:36:26 -0400

    feat(nvim): enable plugin update checker and comment out Neovide settings
    - Enabled periodic plugin update checking in `lazy.lua` by setting `checker.enabled` to `true`.
    - Commented out several Neovide-related settings in `neovide.lua` to simplify configuration or disable specific features.

--- nvim/.config/nvim/lua/config/neovide.lua
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -11,12 +11,12 @@ if vim.g.neovide then
     vim.g.snacks_animate = false
 end
 
-vim.g.neovide_cursor_antialiasing = true
+-- vim.g.neovide_cursor_antialiasing = true
 vim.opt.linespace = 0
-vim.g.neovide_refresh_rate = 120
+-- vim.g.neovide_refresh_rate = 120
 vim.g.neovide_fullscreen = true
 -- vim.g.neovide_underline_stroke_scale = 3
-vim.g.neovide_cursor_smooth_blink = true
+-- vim.g.neovide_cursor_smooth_blink = true
 vim.g.neovide_padding_right = centered_padding
 vim.g.neovide_padding_left = centered_padding
 vim.g.neovide_scroll_animation_far_lines = 0
