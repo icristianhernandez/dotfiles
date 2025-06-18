47dac3113fc12ba6f267cd1fbf0e046d5ba75267
cristian <cristianhernandez9007@gmail.com>
2025-06-18 02:33:36 -0400

    feat(nvim): blink enhance trigger behavior and LSP trigger characters
    - Enable suggestions on insert and backspace in keyword for blink.cmp.
    - Refactor LSP provider trigger characters to extend with newline, tab, space, and dash using grouped lists.

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -46,7 +46,11 @@ return {
             keyword = { range = "full" },
 
             -- by default, blink.cmp will block newline, tab and space trigger characters, disable that behavior
-            trigger = { show_on_blocked_trigger_characters = {} },
+            trigger = {
+                show_on_blocked_trigger_characters = {},
+                show_on_insert = true,
+                show_on_backspace_in_keyword = true,
+            },
         },
 
         cmdline = {
@@ -61,13 +65,22 @@ return {
         },
 
         sources = {
-            -- add newline, tab and space to LSP source trigger characters
             providers = {
                 lsp = {
                     override = {
                         get_trigger_characters = function(self)
                             local trigger_characters = self:get_trigger_characters()
-                            vim.list_extend(trigger_characters, { "\n", "\r\n", "\t", " ", "-" })
+                            local char_groups = {
+                                newline = { "\n", "\r\n" },
+                                tab = { "\t" },
+                                space = { " " },
+                                dash = { "-" },
+                            }
+
+                            for _, chars in pairs(char_groups) do
+                                vim.list_extend(trigger_characters, chars)
+                            end
+
                             return trigger_characters
                         end,
                     },
