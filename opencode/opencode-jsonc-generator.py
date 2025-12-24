f5191cfdf8a6dd605f23e572c2d1e564142c8e3a
cristian <129994548+icristianhernandez@users.noreply.github.com>
2025-12-24 18:46:20 -0400

    feat(opencode): added nix eval as allowed command

--- opencode/opencode-jsonc-generator.py
+++ opencode/opencode-jsonc-generator.py
@@ -72,6 +72,7 @@ core_permissions = {
         "nix run ./nixos#nvim-lint": "allow",
         "nix run ./nixos#workflows-fmt": "allow",
         "nix run ./nixos#workflows-lint": "allow",
+        "niv eval *": "allow",
         "rg *": "allow",
         "git status *": "allow",
         "cd *": "allow",
