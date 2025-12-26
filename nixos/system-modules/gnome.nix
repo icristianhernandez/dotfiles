d980ce880089ee67ce5cbbca43db1240f848fe12
cristian <129994548+icristianhernandez@users.noreply.github.com>
2025-12-25 20:58:17 -0400

    feat: add MPV media player config and enhance GNOME multimedia
    - mpv: enable desktop role with MPRIS, UOSC UI, thumbnails, and HW accel
    - gnome: enable tumbler, add file preview, archiving, and GStreamer codecs

--- nixos/system-modules/gnome.nix
+++ nixos/system-modules/gnome.nix
@@ -10,10 +10,29 @@ guardRole "gnome" {
     desktopManager.gnome.enable = true;
     gvfs.enable = true;
     udisks2.enable = true;
+    tumbler.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
+    # select a file and press [Space] to preview without opening
+    sushi
+
+    # Thumbnails for everything (files)
+    webp-pixbuf-loader
+    gnome-epub-thumbnailer
+    ffmpegthumbnailer
+
+    # Archiving in Files
     file-roller
     p7zip
+
+    # GStreamer codecs and plugins
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav # Critical: Uses FFmpeg for most common video formats
+    gst_all_1.gst-vaapi # Hardware acceleration
   ];
 }
