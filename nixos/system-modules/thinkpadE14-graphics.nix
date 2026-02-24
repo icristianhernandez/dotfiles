f4c0c77fd5e54c9e6f128944824ad7430fb67840
cristian <129994548+icristianhernandez@users.noreply.github.com>
2026-02-24 15:36:15 -0400

    chore(thinkpadE14-graphics): update i915 enable_guc kernel param
    - graphics: comment old guc=3, add guc=7 in boot.kernelParams
    - graphics: minor formatting tweaks to Xe driver section

--- nixos/system-modules/thinkpadE14-graphics.nix
+++ nixos/system-modules/thinkpadE14-graphics.nix
@@ -40,20 +40,21 @@ guardRole "thinkpadE14" {
 
   ### changing to i915 drivers
   boot.kernelParams = [
-    "i915.enable_guc=3"
+    # "i915.enable_guc=3"
+    "i915.enable_guc=7"
     "i915.enable_fbc=1"
   ];
 
   #### changing to Xe drivers
-  #boot.initrd.kernelModules = [ "xe" ];
-  #boot.kernelParams = [
-  #  "i915.enable_guc=3"
-  #  "i915.enable_fbc=1"
-  #  "i915.force_probe=!9a49"
-  #  "xe.force_probe=9a49"
-  #];
+  # boot.initrd.kernelModules = [ "xe" ];
+  # boot.kernelParams = [
+  #   "i915.enable_guc=3"
+  #   "i915.enable_fbc=1"
+  #   "i915.force_probe=!9a49"
+  #   "xe.force_probe=9a49"
+  # ];
   #
-  #services.udev.extraRules = ''
-  #  ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x9A49", ATTR{driver_override}="xe"
-  #'';
+  # services.udev.extraRules = ''
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x9A49", ATTR{driver_override}="xe"
+  # '';
 }
