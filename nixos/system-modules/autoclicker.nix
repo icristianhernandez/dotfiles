0729979cf3c4e02328ac5fd3f58d2e930050d5ff
cristian <129994548+icristianhernandez@users.noreply.github.com>
2026-04-09 23:15:14 -0400

    chore(autoclicker): add warning for input-remapper version in stable

--- nixos/system-modules/autoclicker.nix
+++ nixos/system-modules/autoclicker.nix
@@ -5,6 +5,8 @@
 }:
 
 guardRole "gaming" {
+  warnings = pkgs.lib.optional (pkgs.lib.versionAtLeast pkgs.input-remapper.version "2.2.0") "input-remapper in stable nixpkgs reached version ${pkgs.input-remapper.version}. Consider switching back from unstable.input-remapper in autoclicker.nix. The unstable version was used because api changes in the latest version when I do that.";
+
   # In plasma break the cursor aceleration or something when is applied, like if
   # overwrite the value
   services.input-remapper = {
