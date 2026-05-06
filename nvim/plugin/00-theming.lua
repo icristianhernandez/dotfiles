1b531c117ccd8917400b84e24ac8c3db3f3fbb22
cristian <129994548+icristianhernandez@users.noreply.github.com>
2026-05-06 13:15:12 -0400

    refactor(neovim): return to catppucin

--- nvim/plugin/00-theming.lua
+++ nvim/plugin/00-theming.lua
@@ -9,30 +9,35 @@ vim.opt.fillchars:append({ eob = " " })
 require("auto-dark-mode").setup({
     set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
+        -- vim.cmd.colorscheme("everforest")
     end,
     set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
+        -- vim.cmd.colorscheme("catppuccin-nvim")
     end,
 })
 
+if vim.opt.background:get() == "dark" then
+    vim.cmd.colorscheme("catppuccin-nvim")
+else
+    vim.cmd.colorscheme("catppuccin-nvim")
+end
+
 vim.g.everforest_background = "hard"
 vim.g.everforest_enable_italic = true
-vim.cmd.colorscheme("everforest")
 
--- require("catppuccin").setup({
---     term_colors = true,
---     -- transparent_background = true,
---     -- float = {
---     --     -- transparent = true,
---     --     solid = true,
---     -- },
---     background = {
---         light = "latte",
---         dark = "macchiato",
---     },
---     default_integration = false,
---     auto_integrations = true,
---     integrations = {},
--- })
---
--- vim.cmd("colorscheme catppuccin-nvim")
+require("catppuccin").setup({
+    term_colors = true,
+    -- transparent_background = true,
+    -- float = {
+    --     -- transparent = true,
+    --     solid = true,
+    -- },
+    background = {
+        light = "latte",
+        dark = "macchiato",
+    },
+    default_integration = true,
+    auto_integrations = true,
+    -- integrations = {},
+})
