1d20b2f65d96f6cb79e860a067c2bcd574f8c4b7
cristian <129994548+icristianhernandez@users.noreply.github.com>
2026-05-09 19:02:50 -0400

    refactor(nixos): restore uncapped entries in bootloader

--- nixos/system-modules/core.nix
+++ nixos/system-modules/core.nix
@@ -30,8 +30,6 @@ guardRole "base" {
     };
   };
 
-  boot.loader.systemd-boot.configurationLimit = 5;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken.
