2425c37b8158d07b1cc674ca016a922616b19c40
cristian <129994548+icristianhernandez@users.noreply.github.com>
2026-05-14 02:47:37 -0400

    feat(neovim): add neonwave colorscheme, reorder theming setup
    Add base46 and neonwave.nvim packs, setup neonwave (neon intensity, auto background), keep catppuccin as the active colorscheme.

--- nvim/plugin/00-theming.lua
+++ nvim/plugin/00-theming.lua
@@ -2,6 +2,8 @@ vim.pack.add({
     "https://github.com/f-person/auto-dark-mode.nvim",
     { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
     "https://github.com/sainnhe/everforest",
+    "https://github.com/AvengeMedia/base46",
+    "https://github.com/MiladGGG/neonwave.nvim",
 })
 
 vim.opt.fillchars:append({ eob = " " })
@@ -16,12 +18,6 @@ require("auto-dark-mode").setup({
         -- vim.cmd.colorscheme("catppuccin-nvim")
     end,
 })
-
-if vim.opt.background:get() == "dark" then
-    vim.cmd.colorscheme("catppuccin-nvim")
-else
-    vim.cmd.colorscheme("catppuccin-nvim")
-end
 
 vim.g.everforest_background = "hard"
 vim.g.everforest_enable_italic = true
@@ -41,3 +37,18 @@ require("catppuccin").setup({
     auto_integrations = true,
     -- integrations = {},
 })
+
+require("neonwave").setup({
+    intensity = "neon", -- 'soft' or 'neon'
+    transparent_background = false,
+    background = "auto",
+})
+
+-- for some reason that doesn't work in light mode?
+-- vim.cmd.colorscheme("neonwave")
+
+if vim.opt.background:get() == "dark" then
+    vim.cmd.colorscheme("catppuccin-nvim")
+else
+    vim.cmd.colorscheme("catppuccin-nvim")
+end
